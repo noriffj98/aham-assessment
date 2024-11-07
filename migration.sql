90dbd4e723f1908b333fac0182896a8aa52e6747
noriffj98 <noriffahj@gmail.com>
2024-11-07 13:26:14 +0800

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -1,12 +1,13 @@
 /* 
 Task 5: SQL Data Migration - This SQL script migrates data from a JSON file into the SQL database schema.
 */
+
 -- Load JSON data from the file into a variable
 DECLARE @json_data NVARCHAR(MAX);
 
 SELECT @json_data = BulkColumn
 FROM OPENROWSET(
-    BULK 'G:\My Drive\iffah\Test\funds.json',  -- Adjust the path to your JSON file
+    BULK 'G:\My Drive\iffah\Test\AHAM_assessment\funds.json', -- Adjust the path to the JSON file
     SINGLE_CLOB
 ) AS datasource;
 
@@ -17,7 +18,7 @@ FROM OPENJSON(@json_data) AS fund
 WHERE JSON_VALUE(fund.value, '$.fund_manager_name') IS NOT NULL
     AND JSON_VALUE(fund.value, '$.fund_manager_name') NOT IN (SELECT name FROM FundManagers);
 
--- Step 3: Insert Data into Funds Table
+-- Insert Data into Funds Table
 INSERT INTO Funds (id, name, manager_name, description, nav, created_at, performance)
 SELECT
     JSON_VALUE(fund.value, '$.fund_id') AS id,
@@ -28,3 +29,7 @@ SELECT
     JSON_VALUE(fund.value, '$.fund_creation_date') AS created_at,
     JSON_VALUE(fund.value, '$.fund_performance') AS performance
 FROM OPENJSON(@json_data) AS fund;
+
+-- View Tables
+SELECT * FROM FundManagers;
+SELECT * FROM Funds;
